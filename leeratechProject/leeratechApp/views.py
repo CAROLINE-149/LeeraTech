d60b66176d7ed526129dfb6c46f3d25ff0029831
Caroline <caroonyango9@gmail.com>
2026-03-13 13:25:01 +0300

    Setup of the environment

---
+++ leeratechProject/leeratechApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    context = {} #dict that will hold out data
+    return render(request, 'home.html', context)
+
