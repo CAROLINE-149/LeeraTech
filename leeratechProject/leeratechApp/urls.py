0edd2b4ae6ee21647bc7f83afb42a019e9626eec
Caroline <caroonyango9@gmail.com>
2026-03-16 22:51:32 +0300

    creation of html files

--- leeratechProject/leeratechApp/urls.py
+++ leeratechProject/leeratechApp/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('features/',views.features, name='features'),
+    path('about/',views.about, name='about'),
+    path('solutions/',views.solutions, name='solutions'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('contact/', views.contact, name='contact')
 ]
--- leeratechProject/leeratechApp/views.py
+++ leeratechProject/leeratechApp/views.py
@@ -6,3 +6,22 @@ def home(request):
     context = {} #dict that will hold out data
     return render(request, 'leeratechApp/home.html', context)
 
+def features(request):
+    context={}
+    return render(request, 'leeratechApp/features.html', context)
+
+def about(request):
+    context={}
+    return render(request, 'leeratechApp/about.html',context)
+
+def solutions(request):
+    context={}
+    return render(request, 'leeratechApp/solutions.html', context)
+
+def pricing(request):
+    context={}
+    return render(request, 'leeratechApp/pricing.html',context)
+
+def contact(request):
+    context={}
+    return render(request, 'leeratechApp/contact.html',context)
